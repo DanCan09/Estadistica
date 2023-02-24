e4afba8a526645b3588f7a493713c821a7ca2397
Daniela Canabal <daniela.canabal09@gmail.com>
2023-02-23 21:56:13 -0600

    update

---
+++ Scrips/Tarea02_DanielaCanabal.R
@@ -0,0 +1,12 @@
+# Daniela Alexandra Canabal Valdes 
+# 23/02/2023
+# Maestria Ciencias Forestales primer semestre
+
+# HW_02
+# Asignacion 2: Uso de restricciones y estadistica descriptiva
+
+
+
+# Importar datos de trabajo -----------------------------------------------
+
+#Los datos de trabajo provienen del libro Introductory probability & statistics, applications for forestry & natural sciences de Kozak et al. (2008). El Cuadro 1 muestra un ejemplo de datos coletados de 50 árboles con siete variables. (i) Árbol se refiere al número de árbol, (ii) Fecha: mes decolecta en Marzo 2006; (iii) Especies: C: Cedro Rojo (Western red cedar); F: Douglasia verde (Douglas fir); H: Tsuga heterófila (western hemlock); (iv) Posición: clasificación de la copa: D: Dominante, C: codominate, I: Intermedio, S: suprimido; (v) Vecinos: número de vecinos en un radio de 5m; (vi) Diámetro: diámetro a la altura de pecho (1.3m); (vii) Altura total.
